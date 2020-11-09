568de28aaf9227abd9e0998dd1f99fe891c1d829
SushilPudke <74047164+SushilPudke@users.noreply.github.com>
2020-11-09 19:39:08 +0530

    Add files via upload

--- Prg1.c
+++ Prg1.c
@@ -1,10 +1,15 @@
-/* A pro for item bill */
+/* A pro for greeting user
+Hi Everyone
+*/
 #include <stdio.h>
 #include<conio.h>
 
 void main()
 {
-	printf("Hello, world\n");
+	char nm[10];
+	printf("Enter User Name :");
+	scanf("%s",&nm);
+	printf("Hello\t%s\n",nm);
 	
 
 }
